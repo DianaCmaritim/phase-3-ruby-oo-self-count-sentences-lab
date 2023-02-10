560fc483474c5384dba8c9c6699f5d0ec6b6ae58
Diana <maritimdiane96@gmail.com>
2023-02-10 20:18:42 +0300

    completed assignment

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,25 @@
 require 'pry'
 
 class String
+  attr_reader :count_sentences
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    sentences = self.split(/[.?!] /)
+    sentences.count
+    #binding.pry
   end
 end
+
+"Hi, my name is Sophie.What's your name?Happy Halloween!"
